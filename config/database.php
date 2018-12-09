429e4a7764b8c60ab2c00173846fa4cd01de060d
aleksandrazaremba <aleksandrazarem@gmail.com>
2018-12-09 21:24:37 +0000

    regte

--- config/database.php
+++ config/database.php
@@ -42,11 +42,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'eu-cdbr-west-02.cleardb.net'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'heroku_5514aac1eeb8b61'),
+            'username' => env('DB_USERNAME', 'b7967ea97ace52'),
+            'password' => env('DB_PASSWORD', 'e696cd17'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
