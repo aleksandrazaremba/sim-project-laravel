049e8f9c09b8a3882cd98c5d58f10da593de2738
aleksandrazaremba <aleksandrazarem@gmail.com>
2018-12-09 21:43:24 +0000

    check

--- config/database.php
+++ config/database.php
@@ -42,12 +42,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'eu-cdbr-west-02.cleardb.net'),
-            'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'heroku_5514aac1eeb8b61'),
-            'username' => env('DB_USERNAME', 'b7967ea97ace52'),
-            'password' => env('DB_PASSWORD', 'e696cd17'),
-            'unix_socket' => env('DB_SOCKET', ''),
+            'host' => 'eu-cdbr-west-02.cleardb.net',
+            'port' => '3306',
+            'database' => 'heroku_5514aac1eeb8b61',
+            'username' => 'b7967ea97ace52',
+            'password' => 'e696cd17',
+            'unix_socket' => '',
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
