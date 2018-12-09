d346cbc432ad6ea38f0af18437e121b66c0e7c4b
aleksandrazaremba <aleksandrazarem@gmail.com>
2018-12-09 22:01:58 +0000

    commit testowy

--- config/app.php
+++ config/app.php
@@ -14,7 +14,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => 'System to Predict Fertility (SPF)',
 
     /*
     |--------------------------------------------------------------------------
